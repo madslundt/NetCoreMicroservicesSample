d14e1c1fb83bb2cf7c06757b903b75c7e5be1660
Mads Engel Lundt <madslundt@live.dk>
2020-08-28 14:20:04 +0200

    Correct projection

--- Infrastructure/EventStores/Projection/Projection.cs
+++ Infrastructure/EventStores/Projection/Projection.cs
@@ -7,13 +7,13 @@ namespace Infrastructure.EventStores.Projection
 {
     public abstract class Projection : IProjection
     {
-        private readonly Dictionary<Type, Action<object>> Handlers = new Dictionary<Type, Action<object>>();
+        private readonly Dictionary<Type, Action<IEvent>> Handlers = new Dictionary<Type, Action<IEvent>>();
 
         public Type[] Handles => Handlers.Keys.ToArray();
 
-        protected virtual void Projects<TEvent>(Action<TEvent> action) where TEvent : IEvent
+        protected virtual void Projects<TEvent>(Action<IEvent> action)
         {
-            Handlers.Add(typeof(TEvent), @event => action((TEvent)@event));
+            Handlers.Add(typeof(IEvent), @event => action(@event));
         }
 
         public virtual void Handle(IEvent @event)
