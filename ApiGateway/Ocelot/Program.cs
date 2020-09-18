20439c7e60d4ce729a155e043a380cd33c505cf7
Jamie Bonnett <jbonnett@hotmail.co.uk>
2020-09-18 03:56:23 +0100

    fixed gateway

--- ApiGateway/Ocelot/Program.cs
+++ ApiGateway/Ocelot/Program.cs
@@ -7,7 +7,7 @@ namespace ApiGateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
